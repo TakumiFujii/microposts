124daff80ef60600188dd16ecb3b53d8f98061ca
Takumi Fujii <kumm.fj777@gmail.com>
2017-07-30 12:50:50 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  
+  #ログイン機能のやつ
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  #resources :users, only: [:new, :create, :destroy]って書いてもok
+
+  #トップページ
   root to: "toppages#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  #ユーザ登録のやつ
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
